4b64f3b2696635ebf7281834652b83cf55e3a722
J Harsha Vardhan <jharshavardhan28@gmail.com>
2026-02-21 21:03:07 +0530

    Add files via upload

---
+++ scores.py
@@ -0,0 +1,22 @@
+import numpy as np
+np.random.seed(42)
+scores=np.random.randint(50,101,size=(5,4))
+print("3rd student, 2nd student:",scores[2,1])
+print("Last student\n",scores[-2:])
+print("First 3 students, Subjects 2 & 3:\n",scores[:3,1:3])
+column_mean=np.round(np.mean(scores,axis=0),2)
+print("Column-wise mean:",column_mean)
+curve=np.array([5,3,7,2])
+curved_scores=scores+curve
+curved_scores=np.clip(curved_scores,None,100)
+print("Curved Scores:\n",curved_scores)
+row_max=np.max(curved_scores,axis=1)
+print("Row-wise max:",row_max)
+row_min=np.min(curved_scores,axis=1,keepdims=True)
+row_max=np.max(curved_scores,axis=1,keepdims=True)
+normalized=(curved_scores-row_min)/(row_max-row_min)
+print("Normalized Scores:\n",normalized)
+max_Index=np.unravel_index(np.argmax(normalized),normalized.shape)
+print("Highest value at(student, subject):",max_Index)
+above_90=curved_scores[curved_scores>90]
+print("Scores Above 90:",above_90)
